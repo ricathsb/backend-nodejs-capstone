affa6a97e8656d7524db30d3d42f1b7963010400
Richard Fajar Christian Hasibuan <114730591+ricathsb@users.noreply.github.com>
2026-09-18 01:36:36 +0700

    Fix formatting and add newline at end of file

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -13,7 +13,7 @@ const collectionName = 'secondChanceItems'
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
-async function loadData() {
+async function loadData () {
   const client = new MongoClient(url)
 
   try {
